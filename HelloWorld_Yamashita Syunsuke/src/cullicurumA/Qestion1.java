7ae213b2e71df0162392c5e4dccf9c852b205fd7
yamas <yamas@10.29.120.222>
2025-10-02 18:40:22 +0900

    2025.10.02 修正

--- HelloWorld_Yamashita Syunsuke/src/cullicurumA/Qestion1.java
+++ HelloWorld_Yamashita Syunsuke/src/cullicurumA/Qestion1.java
@@ -1,43 +1,48 @@
 package cullicurumA;
 
 public class Qestion1 {
+    public static void main (String... args) {
+        // Q1　各型の変数を宣言し、初期値を設定してください。					
+        // byte 	変数byteNumの初期値を設定		
+        byte number1 = 0;
+        // short 	変数shortNumの初期値を設定	
+         short number2 = 0;
+        // int	変数intNumの初期値を設定		
+         int number3 =0;
+        // long 	変数longNumの初期値を設定	
+         long number4 =0L;
+        // float	変数floatNumの初期値を設定	
+         float number5 = 0.0f;
+        // double	変数doubleNumの初期値を設定	
+         double number6 = 0.0d;
+        // char	変数letterの初期値を設定	
+         char letter = '\u0000';
+        // String 	変数lettersの初期値を設定		
+         String letters = null;
+        // boolean	変数isBooleanの初期値を設定				
+         boolean isBoolean = false;
+         
+         
+        // Q2 1で宣言した各型の変数に指定された値を代入してください					
+            // 変数byteNumに10を代入		
+          number1 = 10;
+            // 変数shortNumに100を代入		
+          number2 = 100;	
+            // 変数intNumに1000を代入	
+          number3 =1000;	
+            // 変数longNumに10000を代入	
+          number4 =10000;	
+            // 変数floatNumに9.5fを代入
+          number5 = 9.5f;
+            // 変数doubleNumに10.5を代入	
+          number6 = 10.5;	
+            // 変数letterに"a"を代入		
+          letter = 'a';	
+            // 変数lettersに"ハロー"を代入		
+          letters = "ハロー";	
+            // 変数isBooleanにtrueを代入
+          isBoolean = true;
 
-//各型の変数を宣言し、初期値を設定してください。					
-//byte 	変数byteNumの初期値を設定		
- byte number1 = 0;
-//short 	変数shortNumの初期値を設定	
- short number2 = 0;
-//int	変数intNumの初期値を設定		
- int number3 =0;
-//long 	変数longNumの初期値を設定	
- long number4 =0L;
-//float	変数floatNumの初期値を設定	
- float number5 = 0.0f;
-//double	変数doubleNumの初期値を設定	
- double number6 = 0.0d;
-//char	変数letterの初期値を設定	
- char letter = '\u0000';
-//String 	変数lettersの初期値を設定		
- String letters = null;
-//boolean	変数isBooleanの初期値を設定				
- boolean isBoolean = false;
-//1で宣言した各型の変数に指定された値を代入してください					
-    //変数byteNumに10を代入		
-  byte number7 = 10;
-    //変数shortNumに100を代入		
-  short number8 = 100;	
-    //変数intNumに1000を代入	
-  int number9 =1000;	
-    //変数longNumに10000を代入	
-  long number10 =10000;	
-    //変数floatNumに9.5fを代入
-  float number11 = 9.5f;
-    //変数doubleNumに10.5を代入	
-  double number12 = 10.5;	
-    //変数letterに"a"を代入		
-  char letter2 = 'a';	
-    //変数lettersに"ハロー"を代入		
-  String letters2 = "ハロー";	
-    //変数isBooleanにtrueを代入
-  boolean isBoolean2 = true;
+
+    }
 }
