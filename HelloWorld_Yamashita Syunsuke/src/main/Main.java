f50446d84274e2c4dc6292ce27ddd0314833b8e9
yamas <yamas@10.29.120.210>
2025-10-15 10:32:41 +0900

    修正

--- HelloWorld_Yamashita Syunsuke/src/main/Main.java
+++ HelloWorld_Yamashita Syunsuke/src/main/Main.java
@@ -7,17 +7,18 @@ import animal.Animal;
 import greeting.Greeting; // Greetingクラスをインポート
 
 public class Main {
-    public static void main(String[] args) {
-        Greeting greeting = new Greeting();
-        greeting.sayHello();
-        
-        Animal lion = new Animal();
-        lion.setName("ライオン");
-        lion.setLength(2.1);
-        lion.setSpeed(80);
-        System.out.println("動物名：" + lion.getName());
-        System.out.println("体長：" + lion.getLength() + "m");
-        System.out.println("速度：" + lion.getSpeed() + "km/h");
-    }
-    
+	public static void main(String[] args) {
+		Greeting greeting = new Greeting();
+		greeting.sayHello();
+		
+		//Q5
+		Animal lion = new Animal();
+		lion.setName("ライオン");
+		lion.setLength(2.1);
+		lion.setSpeed(80);
+		System.out.println("動物名：" + lion.getName());
+		System.out.println("体長：" + lion.getLength() + "m");
+		System.out.println("速度：" + lion.getSpeed() + "km/h");
+	}
+
 }
