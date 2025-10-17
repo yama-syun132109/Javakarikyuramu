694620a5b6fa5729f6178e46ed4f17ca7ef00d8e
yamas <yamas@10.29.121.195>
2025-10-17 18:31:51 +0900

    curriculumC

--- HelloWorld_Yamashita Syunsuke/src/curriculumC/Main.java
+++ HelloWorld_Yamashita Syunsuke/src/curriculumC/Main.java
@@ -1,4 +1,4 @@
-package curriculum30;
+package curriculumC;
 
 public class Main {
     public static void main(String[] args) {
--- HelloWorld_Yamashita Syunsuke/src/curriculumC/Person.java
+++ HelloWorld_Yamashita Syunsuke/src/curriculumC/Person.java
@@ -1,4 +1,4 @@
-package curriculum30;
+package curriculumC;
 
 public class Person {
     // インスタンスフィールド
